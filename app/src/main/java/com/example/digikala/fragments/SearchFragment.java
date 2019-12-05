e1889ba4bdf12918c6dd148fdb5219c4ccc516da
mohammadreza <you@example.com>
2019-12-05 09:37:32 -0800

    /edit the search view in fragment search

--- app/src/main/java/com/example/digikala/fragments/SearchFragment.java
+++ app/src/main/java/com/example/digikala/fragments/SearchFragment.java
@@ -13,11 +13,18 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.digikala.R;
@@ -34,9 +41,10 @@ import Woo.Repository.Repository;
  */
 public class SearchFragment extends Fragment {
     private WooCommerce mWooCommerce = new WooCommerce();
-    private SearchView searchView;
+    private EditText searchEditText;
     private RecyclerView mRecyclerView;
     private ImageButton mArrowButton;
+    private ImageView mClearEditText;
     public static SearchFragment newInstance() {
 
         Bundle args = new Bundle();
@@ -65,36 +73,71 @@ public class SearchFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_search, container, false);
-        searchView = view.findViewById(R.id.fragment_search_search_view);
-        mRecyclerView = view.findViewById(R.id.fragment_search_recycler);
-        mArrowButton=view.findViewById(R.id.fragment_search_toolbar_arrow);
+        searchEditText = view.findViewById(R.id.fragment_search_search_edit_text);
+        mArrowButton = view.findViewById(R.id.fragment_search_toolbar_arrow);
+        mClearEditText=view.findViewById(R.id.fragment_search_clear_text);
         mArrowButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 getActivity().finish();
             }
         });
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//            @Override
+//            public boolean onQueryTextSubmit(String query) {
+//                SearchAsynceTask searchAsynceTask = new SearchAsynceTask();
+//                searchAsynceTask.execute(query);
+//                SharedPreferencesData.setQuery(getActivity(), query);
+//                return true;
+//            }
+//
+//            @Override
+//            public boolean onQueryTextChange(String newText) {
+//                return false;
+//            }
+//        });
+        searchEditText.addTextChangedListener(new TextWatcher() {
             @Override
-            public boolean onQueryTextSubmit(String query) {
-                SearchAsynceTask searchAsynceTask = new SearchAsynceTask();
-                searchAsynceTask.execute(query);
-                SharedPreferencesData.setQuery(getActivity(),query);
-                return true;
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
             }
 
             @Override
-            public boolean onQueryTextChange(String newText) {
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.length()>1){
+                    mClearEditText.setVisibility(View.VISIBLE);
+
+                }else {
+                    mClearEditText.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        mClearEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                searchEditText.setText("");
+            }
+        });
+        searchEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_SEARCH) {
+                    SearchAsynceTask searchAsynceTask = new SearchAsynceTask();
+                    searchAsynceTask.execute(textView.getText().toString());
+                    SharedPreferencesData.setQuery(getActivity(), textView.getText().toString());
+                    return true;
+                }
                 return false;
             }
         });
-
-
         return view;
     }
-
-
 
 
     private class SearchAsynceTask extends AsyncTask<String, String, Void> {
@@ -116,8 +159,8 @@ public class SearchFragment extends Fragment {
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
             if (Repository.getInstance().getSearchedProducts() != null) {
-               Intent intent= ListProductsActivity.newIntent(getActivity(),4);
-               startActivity(intent);
+                Intent intent = ListProductsActivity.newIntent(getActivity(), 4);
+                startActivity(intent);
             } else {
                 Toast.makeText(getActivity(), "موردی یافت نشد", Toast.LENGTH_LONG).show();
             }
