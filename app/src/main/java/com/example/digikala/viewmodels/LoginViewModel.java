d85f7dec52ef834de888f78d6f52f4cdefc28a3b
mohammadreza <you@example.com>
2019-12-16 07:02:35 -0800

    +create signup fragment and make their view and made login view model

---
+++ app/src/main/java/com/example/digikala/viewmodels/LoginViewModel.java
@@ -0,0 +1,12 @@
+package com.example.digikala.viewmodels;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class LoginViewModel extends AndroidViewModel {
+    public LoginViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
--- app/src/main/java/com/example/digikala/views/activities/LoginActivity.java
+++ app/src/main/java/com/example/digikala/views/activities/LoginActivity.java
@@ -1,8 +1,13 @@
 package com.example.digikala.views.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+
+import com.example.digikala.R;
+import com.example.digikala.views.fragments.LoginFragment;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        FragmentManager fm=getSupportFragmentManager();
+        Fragment fragment=fm.findFragmentById(R.id.login_activity_container);
+        if(fragment==null)
+        {
+            fm.beginTransaction().replace(R.id.login_activity_container, LoginFragment.newInstance())
+                    .commit();
+        }
+
     }
 }
